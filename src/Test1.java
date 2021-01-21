8b37f5b9729868070e629b8bd9fb0eb121b5c57d
Amitabh.Pandey <Amitabh.Pandey@IN2278201W1.ey.net>
2021-01-21 21:26:33 +0530

    Project Initiated

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungery="YES";
+		if(hungery=="YES") {
+			System.out.println("You are hungery.Eat food..");
+		}
+		else {
+			System.out.println("Do Work..");
+		}
+	}
+
+}
